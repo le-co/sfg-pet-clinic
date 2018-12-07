45ee57198db631f43776478bd4b2b4e04f1de892
leco <lcosta.sp.br@gmail.com>
2018-12-06 22:16:40 -0200

    using model and view

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -5,9 +5,11 @@ import guru.springframework.sfgpetclinic.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -31,16 +33,16 @@ public class OwnerController {
         return "notimplemented";
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String findById(Model model, @PathVariable Integer id) {
+    @GetMapping(value = "/{id}")
+    public ModelAndView findById(@PathVariable Integer id) {
+
+        ModelAndView mav = new ModelAndView("owners/show");
 
         Owner owner = this.ownerService.findById(id);
-        if (owner == null) {
-            return "redirect:error/404";
-        }
 
-        model.addAttribute("owner", owner);
-        return "owners/show";
+        mav.addObject("owner", owner);
+
+        return mav;
     }
 
 }
